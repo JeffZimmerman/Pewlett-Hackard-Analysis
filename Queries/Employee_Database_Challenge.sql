2423b9db2350342fde352c94c9f5e547e3edb916
Jeff Zimmerman <91562577+elchubasco@users.noreply.github.com>
2021-12-05 21:29:43 -0500

    Add files via upload

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -1,5 +1,6 @@
 -- Employee_Database_challenge
 
+-- Deliverable 1
 -- Create Retirement Titles table
 SELECT e.emp_no,
     e.first_name, 
@@ -18,18 +19,34 @@ ORDER BY e.emp_no;
 SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
 	rt.first_name,
 	rt.last_name,
-	rt.title
+	rt.title,
+    rt.to_date
 INTO unique_titles
 FROM retirement_titles as rt
-ORDER BY rt.emp_no, title DESC;
+ORDER BY rt.emp_no, to_date DESC;
 
 -- Create Retiring Titles table
-SELECT COUNT(ut.emp_no), 
+SELECT COUNT(ut.title), 
 	ut.title
 INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY title
 ORDER BY COUNT (title) DESC;
-SELECT * FROM retiring_titles;
 
-
+-- Deliverable 2
+-- Create Mentorship Eligibility table
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO membership_eligibility
+FROM employees as e
+Left outer join dept_emp as de
+ON (e.emp_no = de.emp_no)
+Left outer join titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '01-01-1965' AND '12-31-1965')
+ORDER BY e.emp_no;
